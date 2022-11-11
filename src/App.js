407af543bf078c5c6f71444042e6edfd6412b43e
Iaroslav Uliantcev <doudou239@gmail.com>
2022-11-11 13:44:48 +0300

    Two variants of smooth transition for linear-gradient based on change of opacity of before

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import styled from 'styled-components'
+
+const Basement = styled.div`
+  display: flex;
+`
+const Block1 = styled.div`
+  --color1: green;
+  --color2: blue;
+  position: relative;
+  display: block;
+  box-sizing: border-box;
+  width: 300px;
+  height: 300px;
+  margin: 100px;
+  color: #61dafb;
+  background: linear-gradient(var(--color2), var(--color1));
+  transition: --color1 3s, --color2 3s;
+  ::before {
+      position: absolute;
+      content: "";
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background-image: linear-gradient(
+        to bottom,
+        hsl(344, 100%, 50%),
+        hsl(31, 100%, 40%)
+      );
+      //z-index: -1;
+      transition: opacity 0.5s linear;
+      opacity: 0.1; 
+  }
+  :hover::before{
+    opacity: 1;
+  }
+`
+
+const Block2 = styled.div`
+  position: relative;
+  display: block;
+  height: 300px;
+  width: 300px;
+  margin: 100px;
+  color: #61dafb;
+  background: linear-gradient(45deg, green, blue, green);
+  ::before {
+      position: absolute;
+      content: "";
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background-image: linear-gradient(45deg, blue, green, blue);
+      //z-index: -1;
+      transition: opacity 0.5s linear;
+      opacity: 0; 
+  }
+  &.alter{
+    ::before{
+      opacity: 1;
+    }
+  }
+`
 
 function App() {
+    const [inProp, setInProp] = useState(false)
+
+    useEffect(()=>{
+        console.log("inProp", inProp ? "true" : "false")
+    },[inProp])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Basement className="App">
+        <Block1>Hover me</Block1>
+      <Block2
+          className={inProp && "alter"}
+          onClick = {()=>{setInProp(!inProp)}}
+      >Click me</Block2>
+    </Basement>
   );
 }
 
